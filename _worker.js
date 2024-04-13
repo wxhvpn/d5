5e7ebe3ec88d61c1ce9d7d0eaf1be94c05626f74
git <41714976+wxhvpn@users.noreply.github.com>
2024-04-13 20:42:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -29,7 +29,7 @@ let addressesnotlsapi = [
 
 let DLS = 10;//速度下限
 let addressescsv = [
-    'https://raw.githubusercontent.com/wxhvpn/d5/main/396982-1712946770.csv', 
+    'https://raw.githubusercontent.com/wxhvpn/d2/main/addressesapi.csv', 
 ];
 
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
